918775758be799b5834a002f9e4934c10ff7eef8
Dhanesh <dhaneshreddy470@gmail.com>
2026-06-15 14:28:32 +0000

    fixed snap chromium jenkins issue

--- GoogleTest.java
+++ GoogleTest.java
@@ -12,16 +12,18 @@ public class GoogleTest {
 
         ChromeOptions options = new ChromeOptions();
 
-        // IMPORTANT: headless mode for Jenkins
-        options.addArguments("--headless");
+        options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
+
+        // SNAP chromium fix (IMPORTANT)
+        options.setBinary("/snap/bin/chromium");
 
         WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://www.google.com");
 
-        System.out.println("Title: " + driver.getTitle());
+        System.out.println(driver.getTitle());
 
         driver.quit();
     }
