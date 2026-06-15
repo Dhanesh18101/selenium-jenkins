8302f7b3b4e3804edd756b305d7df98b40bb4119
Dhanesh <dhaneshreddy470@gmail.com>
2026-06-15 14:38:07 +0000

    fixed selenium webdriver issue using WebDriverManager

--- GoogleTest.java
+++ GoogleTest.java
@@ -4,22 +4,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class GoogleTest {
 
     @Test
     public void openGoogle() {
 
-        ChromeOptions options = new ChromeOptions();
+        WebDriverManager.chromedriver().setup();
 
+        ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-
-        // IMPORTANT: DO NOT set snap path
-        options.setBinary("/usr/bin/chromium");
-
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 
         WebDriver driver = new ChromeDriver(options);
 
