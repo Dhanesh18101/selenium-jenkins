1b5938527aaa1cf85052ea8fb9b02a74b8d978f6
Dhanesh <dhaneshreddy470@gmail.com>
2026-06-16 05:38:22 +0000

    fixed selenium headless issue for jenkins

--- src/test/java/com/test/GoogleTest.java
+++ src/test/java/com/test/GoogleTest.java
@@ -2,6 +2,7 @@ package com.test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class GoogleTest {
@@ -9,12 +10,18 @@ public class GoogleTest {
     @Test
     public void openGoogle() {
 
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        ChromeOptions options = new ChromeOptions();
 
-        WebDriver driver = new ChromeDriver();
+        // IMPORTANT FOR JENKINS / WSL
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://www.google.com");
 
-        System.out.println("Google opened successfully");
+        System.out.println("Google opened in headless mode");
 
         driver.quit();
     }
